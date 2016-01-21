48b1f7598a8c079064016cf928b7d1c532339b02
Kevin Malby <kevin.malby@gmail.com>
2016-01-20 23:01:52 -0800

    Change port to abide to Heroku Rules

--- app.js
+++ app.js
@@ -5,9 +5,13 @@ let express = require('express'),
 
 let app = express();
 
+app.set('port', (process.env.PORT || 5000));
+
 require('./routes')(app);
 app.use(bodyParser.urlencoded({
 	extended: false
 }));
 
-app.listen(3000);
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
